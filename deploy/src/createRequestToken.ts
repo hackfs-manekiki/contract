60224f0d571b499dfa38416f9b2d54f7850e1999
apemon <760108+apemon@users.noreply.github.com>
2022-07-23 14:12:11 +0700

    update reqest method

--- deploy/src/createRequestToken.ts
+++ deploy/src/createRequestToken.ts
@@ -1,5 +1,6 @@
 import { ethers } from 'ethers'
 import * as VaultArtifact from '../../out/Vault.sol/Vault.json'
+import * as TokenArtifact from '../../out/MockToken.sol/MockToken.json'
 
 async function main() {
     const privateKey = '0x2a871d0798f97d79848a013d4936a73bf4cc922c825d33c1cf7073dff6d409c6'
@@ -7,20 +8,27 @@ async function main() {
 
     const provider = ethers.providers.getDefaultProvider('http://localhost:8545')
     const wallet = new ethers.Wallet(privateKey, provider)
+    // mock token
+    const tokenFactory = new ethers.ContractFactory(TokenArtifact.abi, TokenArtifact.bytecode.object, wallet)
+    const token = await tokenFactory.deploy('USD', 'USD', 6)
+    console.log(`token: ${token.address}`)
+    await token.mint(vaultAddress, 100000_000000)
+    // vault
     const vault = new ethers.Contract(vaultAddress, VaultArtifact.abi, wallet)
-    console.log(ethers.utils.parseEther('1').toString())
-    const requestEther = {
+    let tokenInterface = new ethers.utils.Interface(TokenArtifact.abi)
+    let data = tokenInterface.encodeFunctionData('transfer', [wallet.address, '1000000000'])
+    const requestToken = {
         requester: wallet.address,
-        to: wallet.address,
-        requestType: '0', // TRANSFER
-        value: ethers.utils.parseEther('1').toString(),
+        to: token.address,
+        requestType: '1', // EXECUTE
+        value: '0',
         budget: '1000000000',
-        data: '0x',
-        name: 'test transfer ether',
+        data,
+        name: 'test transfer token',
         detail: 'hello world',
         attachments: 'ipfs://'
     }
-    await vault.requestApproval(requestEther)
+    await vault.requestApproval(requestToken)
 }
 
 main()
